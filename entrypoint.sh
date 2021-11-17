27302c347a37c3d8d65804f5e4ae11eb1bac107f
fish895623 <dan990429@gmail.com>
2021-11-17 16:03:48 +0900

    sdk version 지정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 # TODO going to fix install platform, sdkversion
-echo "::set-output name=version::$version"
+echo "$1"
+version=$1
 
 sh -c '/commandlinetools/cmdline-tools/bin/sdkmanager --sdk_root=/sdk "platforms;android-$version"'
 echo "SDK installed"
