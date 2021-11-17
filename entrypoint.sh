916432a4d48c529fb76d29a1784df3c608b38aa9
fish895623 <dan990429@gmail.com>
2021-11-17 15:28:37 +0900

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 # TODO going to fix install platform, sdkversion
 time=$(date)
-/commandlinetools/cmdline-tools/bin/sdkmanager --sdk_root=/sdk "platforms;android-$version"
+sh -c '/commandlinetools/cmdline-tools/bin/sdkmanager --sdk_root=/sdk "platforms;android-$version"'
 echo "SDK installed"
